05aaa85fae8f0ec2608540fdb9ba767b6d689385
NoobMaester <joshstilez@gmail.com>
2023-02-22 11:26:21 +0200

    route parameters

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import {
 import HomePage from "./home/HomePage";
 import "./App.css";
 import ProjectsPage from "./projects/ProjectsPage";
+import ProjectPage from "./projects/ProjectPage";
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/projects/:id" element={<ProjectPage />}/>
         </Routes>
       </div>
     </Router>
--- src/projects/ProjectCard.tsx
+++ src/projects/ProjectCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Project } from "./Project";
+import { Link } from "react-router-dom";
 
 function formatDescription(description: string): string {
   return description.substring(0, 60) + "";
@@ -21,11 +22,13 @@ const ProjectCard = ({ project, onEdit }: ProjectCardProps) => {
     <div className="card">
       <img src={project.imageUrl} alt={project.name} />
       <section className="section dark">
+        <Link to={'/projects/' + project.id}>
         <h5 className="strong">
           <strong>{project.name}</strong>
         </h5>
         <p>{formatDescription(project.description)}</p>
         <p>Budget : {project.budget.toLocaleString()}</p>
+        </Link>
         <button className="bordered" onClick={()=> handleEditClick(project)}>
             <span className="icon-edit"></span>
             Edit
